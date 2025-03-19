7e886e8e5354a986bdbd87f428ca692f06606ef0
@cleversonramos <cleverson.ramos@bistwobis.com.br>
2025-03-19 14:47:59 -0300

    init

---
+++ agenda/src/views/dashboard/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+/*Call Components*/
+// import SalesOverview from '@/components/dashboard/SalesOverview.vue';
+// import YearlyBreakup from '@/components/dashboard/YearlyBreakup.vue';
+// import MonthlyEarning from '@/components/dashboard/MonthlyEarnings.vue';
+// import RecentTransaction from '@/components/dashboard/RecentTransaction.vue';
+// import ProductPerformance from '@/components/dashboard/ProductPerformance.vue';
+// import ProductCards from '@/components/dashboard/ProductCards.vue';
+</script>
+<template>
+    <v-row>
+        
+        <v-col class="text-center mt-2">
+            <p class="text-muted">Design and Developed by <a flat variant="text" href="https://adminmart.com/" target="_blank" class="pl-1 text-primary">AdminMart.com</a></p>
+        </v-col>
+    </v-row>
+</template>
